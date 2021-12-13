369a27b83f3769de387b1a9a527a1eb76bbe8c05
MARIEM <tanazeftimariem9@gmail.com>
2021-12-13 10:51:16 +0100

    First commit

---
+++ src/com/Villa.java
@@ -0,0 +1,10 @@
+package com;
+
+import java.security.PrivateKey;
+
+public class Villa {
+    private int id_villa;
+    private String loc_villa;
+    private int price;
+    private String Desc_Villa;
+}
